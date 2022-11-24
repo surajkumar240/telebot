adfd9300f6250a1f8057cb96706ea3ebce19eaef
surajkumar240 <98449377+surajkumar240@users.noreply.github.com>
2022-11-24 17:58:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,72 @@
-import telegram.ext
-import responses as r
+import telebot
+import json
+import urllib.request
 import os
 from dotenv import load_dotenv
-
+import requests
 load_dotenv()
-TOKEN = os.getenv('TOKEN')
-
-print("Bot started....")
-
-
-def start_command(update, context):
-    update.message.reply_text('Type something')
-
-
-def help_command(update, context):
-    update.message.reply_text('Ask google')
-
-
-def handle_message(update, context):
-    text = str(update.message.text).lower()
-    response = r.sample_responses(text)
-
-    update.message.reply_text(response)
+bot = telebot.TeleBot(os.getenv("TOKEN"), parse_mode="HTML")
+# joke start
+jokeurl = "https://official-joke-api.appspot.com/random_joke"
+jokedata = urllib.request.urlopen(jokeurl).read().decode()
+# parse json object
+jokeobj = json.loads(jokedata)
+joke = jokeobj['setup']
+punchline = jokeobj['punchline']
+# joke end
+commands = {  # command description used in the "help" command
+    'start': 'Start the bot',
+    'help': 'Gives you information about the available commands',
+    'joke': 'Random jokes',
+    'meme': 'Random memes',
+}
 
 
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
-def main():
-    updater = telegram.ext.Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(telegram.ext.CommandHandler("start", start_command))
-    dp.add_handler(telegram.ext.CommandHandler("help", help_command))
-
-    dp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline)
 
 
-main()
+@bot.message_handler(commands=['help'])
+def command_help(m):
+    cid = m.chat.id
+    help_text = "The following commands are available: \n"
+    for key in commands:  # generate help text out of the commands dictionary defined at the top
+        help_text += "/" + key + ": "
+        help_text += commands[key] + "\n"
+    bot.send_message(cid, help_text)  # send the generated help page
+
+
+@bot.message_handler(commands=['joke'])
+def send_joke(message):
+    bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline)
+
+def get_url():
+    contents = requests.get('https://meme-api.herokuapp.com/gimme').json()
+    image_url = contents['url']
+    return image_url
+
+@bot.message_handler(commands=['meme'])
+def msg4(message):
+    url = get_url()
+    bot.send_photo(message.chat.id, url)
+
+@bot.message_handler(commands=['about'])
+def numbermsg(message):
+    msg = bot.reply_to(message, 'Enter Number')
+    bot.register_next_step_handler(msg, number_step)
+def number_step(message):
+    num = message.text
+    answer = requests.get(f'http://numbersapi.com/{num}?json')
+    bot.send_message(message.chat.id, json.loads(answer.text)['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, 'Invalid Command')
+
+
+bot.infinity_polling()
