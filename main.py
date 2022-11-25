63447df65088783408a17d2b59be12517144f54b
surajkumar240 <98449377+surajkumar240@users.noreply.github.com>
2022-11-25 14:22:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from telebot import *
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("TOKEN"))
+
+print("Bot Started..........")
+
 # joke start
 jokeurl = "https://official-joke-api.appspot.com/random_joke"
 jokedata = urllib.request.urlopen(jokeurl).read().decode()
@@ -15,47 +18,22 @@ jokeobj = json.loads(jokedata)
 joke = jokeobj['setup']
 punchline = jokeobj['punchline']
 # joke end
-commands = {  # command description used in the "help" command
-    'start': 'Start the bot',
-    'help': 'Gives you information about the available commands',
-    'joke': 'Random jokes',
-    'meme': 'Random memes',
-    'urlshorten': 'Shorten URLs',
-}
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_first_name = str(message.chat.first_name)
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    btn1 = types.KeyboardButton("/joke")
-    btn2 = types.KeyboardButton("/meme")
-    btn3 = types.KeyboardButton("/urlshorten")
-    btn4 = types.KeyboardButton("/qrgen")
-    btn5 = types.KeyboardButton("/stop")
-    markup.add(btn1, btn2, btn3, btn4, btn5)
-    bot.send_message(chat_id=message.chat.id, text=f"Choose a command {user_first_name}", reply_markup=markup)
+    bot.send_message(message.chat.id, f"Hello {user_first_name}, use the menu to give commands to the bot")
 
 
 @bot.message_handler(commands=['stop'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardRemove(selective=False)
-    bot.send_message(chat_id=message.chat.id, text="Bye!", reply_markup=markup)
+    user_first_name = str(message.chat.first_name)
+    bot.send_message(message.chat.id, text=f"Bye {user_first_name}!")
 
 
 @bot.message_handler(commands=['joke'])
 def send_joke(message):
     bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline, parse_mode="HTML")
-
-
-@bot.message_handler(commands=['help'])
-def command_help(m):
-    cid = m.chat.id
-    help_text = "The following commands are available: \n"
-    for key in commands:  # generate help text out of the commands dictionary defined at the top
-        help_text += "/" + key + ": "
-        help_text += commands[key] + "\n"
-    bot.send_message(cid, help_text)  # send the generated help page
 
 
 @bot.message_handler(commands=['joke'])
@@ -110,6 +88,7 @@ def qrimage(message):
     qrlink = message.text
     image_url = f'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={qrlink}'
     bot.send_photo(message.chat.id, image_url)
+    bot.send_message(message.chat.id, f"QR generated successfully for {qrlink}")
 
 
 @bot.message_handler(func=lambda message: True)
