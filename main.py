0ef495032f10a8142d0c362d8f484f56ce954ca5
surajkumar240 <98449377+surajkumar240@users.noreply.github.com>
2022-11-25 13:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import telebot
 import json
-import urllib.request
 import os
-from dotenv import load_dotenv
+import urllib.request
 import requests
+from dotenv import load_dotenv
+from telebot import *
+
 load_dotenv()
-bot = telebot.TeleBot(os.getenv("TOKEN"), parse_mode="HTML")
+bot = telebot.TeleBot(os.getenv("TOKEN"))
 # joke start
 jokeurl = "https://official-joke-api.appspot.com/random_joke"
 jokedata = urllib.request.urlopen(jokeurl).read().decode()
@@ -19,17 +20,32 @@ commands = {  # command description used in the "help" command
     'help': 'Gives you information about the available commands',
     'joke': 'Random jokes',
     'meme': 'Random memes',
+    'urlshorten': 'Shorten URLs',
 }
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    user_first_name = str(message.chat.first_name)
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    btn1 = types.KeyboardButton("/joke")
+    btn2 = types.KeyboardButton("/meme")
+    btn3 = types.KeyboardButton("/urlshorten")
+    btn4 = types.KeyboardButton("/qrgen")
+    btn5 = types.KeyboardButton("/stop")
+    markup.add(btn1, btn2, btn3, btn4, btn5)
+    bot.send_message(chat_id=message.chat.id, text=f"Choose a command {user_first_name}", reply_markup=markup)
+
+
+@bot.message_handler(commands=['stop'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(chat_id=message.chat.id, text="Bye!", reply_markup=markup)
 
 
 @bot.message_handler(commands=['joke'])
 def send_joke(message):
-    bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline)
+    bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline, parse_mode="HTML")
 
 
 @bot.message_handler(commands=['help'])
@@ -46,24 +62,56 @@ def command_help(m):
 def send_joke(message):
     bot.reply_to(message, '<b>Joke:</b> ' + joke + '\n<b>Punchline:</b> ' + punchline)
 
+
 def get_url():
     contents = requests.get('https://meme-api.herokuapp.com/gimme').json()
     image_url = contents['url']
     return image_url
+
 
 @bot.message_handler(commands=['meme'])
 def msg4(message):
     url = get_url()
     bot.send_photo(message.chat.id, url)
 
+
 @bot.message_handler(commands=['about'])
 def numbermsg(message):
     msg = bot.reply_to(message, 'Enter Number')
     bot.register_next_step_handler(msg, number_step)
+
+
 def number_step(message):
     num = message.text
     answer = requests.get(f'http://numbersapi.com/{num}?json')
     bot.send_message(message.chat.id, json.loads(answer.text)['text'])
+
+
+@bot.message_handler(commands=['urlshorten'])
+def urlmsg(message):
+    longurl = bot.reply_to(message, 'Enter url')
+    bot.register_next_step_handler(longurl, url_step)
+
+
+def url_step(message):
+    lurl = message.text
+    answer = requests.get(f'https://api.shrtco.de/v2/shorten?url={lurl}')
+    surl = json.loads(answer.text)['result']['short_link']
+    bot.send_message(message.chat.id, f"Short URL: {surl}")
+
+
+@bot.message_handler(commands=['qrgen'])
+def qrmsg(message):
+    qrurl = bot.reply_to(message, 'Enter url')
+    bot.register_next_step_handler(qrurl, qrimage)
+
+
+def qrimage(message):
+    qrlink = message.text
+    image_url = f'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={qrlink}'
+    bot.send_photo(message.chat.id, image_url)
+
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, 'Invalid Command')
