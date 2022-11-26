5e344c898a9bc3bba40de3efb2a47305bba60e22
surajkumar240 <98449377+surajkumar240@users.noreply.github.com>
2022-11-26 11:26:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from telebot import *
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv("TOKEN"))
-
-print("Bot Started........")
+happikey = os.getenv("PASSAPI")
+print("Bot Started..........")
 
 # joke start
 jokeurl = "https://official-joke-api.appspot.com/random_joke"
@@ -17,6 +17,8 @@ jokedata = urllib.request.urlopen(jokeurl).read().decode()
 jokeobj = json.loads(jokedata)
 joke = jokeobj['setup']
 punchline = jokeobj['punchline']
+
+
 # joke end
 
 @bot.message_handler(commands=['start'])
@@ -86,9 +88,10 @@ def qrmsg(message):
 
 def qrimage(message):
     qrlink = message.text
-    image_url = f'https://api.qrserver.com/v1/create-qr-code/?data={qrlink}&size=300x300&format=png'
+    image_url = f'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data={qrlink}&format=png'
     bot.send_photo(message.chat.id, image_url)
     bot.send_message(message.chat.id, f"QR generated successfully for {qrlink}")
+
 
 @bot.message_handler(commands=['passwordgen'])
 def passmsg(message):
@@ -98,9 +101,26 @@ def passmsg(message):
 
 def pass_gen(message):
     maxlen = message.text
-    answer = requests.get(f'https://passwordinator.herokuapp.com/?len={maxlen}&char=true&num=true&caps=true')
-    finpass = json.loads(answer.text)['data']
+
+    answer = requests.get(f'https://api.happi.dev/v1/generate-password?apikey={happikey}&limit=1&length={maxlen}&num=1&upper=1&symbols=1')
+    finpass = json.loads(answer.text)['passwords']
     bot.send_message(message.chat.id, f"Password: `{finpass}`", parse_mode="Markdown")
+
+@bot.message_handler(commands=['weather'])
+def weathermsg(message):
+    cityname = bot.reply_to(message, 'Enter city name')
+    bot.register_next_step_handler(cityname, weather_gen)
+
+def weather_gen(message):
+    cityn = message.text
+    WTOKEN = os.getenv("WTOKEN")
+    answer = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{cityn}?unitGroup=metric&key={WTOKEN}&contentType=json')
+    cityl = json.loads(answer.text)['resolvedAddress']
+    maxtemp = json.loads(answer.text)['days'][0]['tempmax']
+    mintemp = json.loads(answer.text)['days'][0]['tempmin']
+    winds = json.loads(answer.text)['days'][0]['windspeed']
+    humidity = json.loads(answer.text)['days'][0]['humidity']
+    bot.send_message(message.chat.id, f"<b>Region:</b> {cityl} \n<b>Maximum Temperature:</b> {maxtemp}°C \n<b>Minimum Temperature:</b> {mintemp}°C \n<b>Windspeed:</b> {winds} km/h \n<b>Humidity:</b> {humidity}%", parse_mode="HTML")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
